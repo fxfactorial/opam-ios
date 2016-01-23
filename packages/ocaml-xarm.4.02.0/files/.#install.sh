7a248894378f4a814b896ad21538d0254d08fc58
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-01-23 13:42:32 -0800

    Update install script

--- packages/ocaml-xarm.4.02.0/files/.#install.sh
+++
@@ -1,1 +0,0 @@
-Edgar@TuringMachine.local.16177
--- packages/ocaml-xarm.4.02.0/files/install.sh
+++ packages/ocaml-xarm.4.02.0/files/install.sh
@@ -4,6 +4,8 @@ PREFIX="$1"
 
 rm -rf "${PREFIX}"/bin "${PREFIX}"/lib "${PREFIX}"/man
 
+sed -i -e s/OCAMLDIR/"${PREFIX}"/g ocamloptrev
+mv ocamloptrev "${PREFIX}"/bin
 mv bin "${PREFIX}"
 mv lib "${PREFIX}"
 mv man "${PREFIX}"
