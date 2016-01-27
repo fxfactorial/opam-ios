e8984c234799763e59bd740eff3e712d567a3b6d
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-01-27 01:09:33 -0800

    Change build strategies.

--- packages/ocaml-xarm.4.02.0/files/install.sh
+++ packages/ocaml-xarm.4.02.0/files/install.sh
@@ -2,12 +2,36 @@
 
 PREFIX="$1"
 
-rm -rf "${PREFIX}"/bin "${PREFIX}"/lib "${PREFIX}"/man
+export XARMTARGET="${PREFIX}"
+
+svn co svn://svn.psellos.com/tags/ocamlxarm-4.0.2 ocamlxarm-4.0 
+cd ocamlxarm-4.0
+
+sed -i bak \
+    -e \
+    's|export XARMTARGET=/usr/local/ocamlxarm|export XARMTARGET='${PREFIX}'|g' \
+    xarm-build
+
+sed -i bak \
+    -e \
+    's|export SDK=/Developer/SDKs/iPhoneOS7.1.sdk|export SDK=/Developer/SDKs/iPhoneOS8.3.sdk|g' \
+    xarm-build
+
+sed -i bak \
+    -e \
+    's|export OSXARCH=i386|export OSXARCH=x86_64|g' \
+    xarm-build
+
+sed -i bak \
+    -e \
+    's|# export ARMARCH=v6|export ARMARCH=v7|g' \
+    xarm-build
+
+sh xarm-build all > xarm-build.log
+
+make install
+
+cd ..
 
 sed -i bak -e 's|OCAMLDIR|'${PREFIX}'|g' ocamloptrev
-
-mv bin "${PREFIX}"
-cp ocamloptrev "${PREFIX}"/bin
-# cp wrapped "${PREFIX}"/bin/ocamlopt
-mv lib "${PREFIX}"
-mv man "${PREFIX}"
+cp ocamloptrev "${PREFIX}/bin"
