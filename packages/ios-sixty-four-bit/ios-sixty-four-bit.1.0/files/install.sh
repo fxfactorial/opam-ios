b266a83c3a6dc236a04c69b7c2f42baddd98ad40
Edgar Aroutiounian <edgar.factorial@gmail.com>
2016-02-12 00:57:26 -0800

    Try iOS 9.2

---
+++ packages/ios-sixty-four-bit/ios-sixty-four-bit.1.0/files/install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -e
+
+prefix="$1"
+bits=64
+first_clone=ocaml-4.02.3-$bits
+second_clone=gs-4.02.3+ios-$bits
+
+git clone -b 4.02.3 https://github.com/ocaml/ocaml ${first_clone}
+
+cd ${first_clone}
+
+sed -i "" "1s/4.02.3/4.02.3+ios/g" VERSION
+./configure -prefix ${prefix}/release
+make world.opt
+make install
+
+PATH=${prefix}/release/bin:${PATH}
+
+cd ..
+
+git clone -b gs-4.02.3+ios https://github.com/gerdstolpmann/ocaml ${second_clone}
+
+cd ${second_clone}
+sed -i "" "1s/4.02.3/4.02.3+ios/g" VERSION
+sed -i "" '315s|`which ocamlrun`|$(BINDIR)/ocamlrun|g' Makefile
+sed -i "" "8s/arch=armv7/arch=arm64/g" build.sh
+sed -i "" '12s|sdk=8.4|SDK=/Developer/SDKs/${platform}.sdk|' build.sh
+sed -i "" '7s/.*/minver=7.0/g' build.sh
+sed -i "" 's/-miphoneos-version-min=$sdk/-miphoneos-version-min=$minver/g' build.sh
+
+./build.sh -prefix ${prefix} -target-bindir ${prefix}/bin
+make install
---
+++ packages/ios-thirty-two-bit/ios-thirty-two-bit.1.0/files/remove.sh
@@ -0,0 +1,4 @@
+#!bin/sh -e
+
+PREFIX="$1"
+
